240479f917270cc9dc509deaa2ce7291b78d6fea
ohokusan <ohoku@yahoo.com>
2024-01-01 21:32:39 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+/*
+1 meter = 3.281 feet
+1 liter = 0.264 gallon
+1 kilogram = 2.204 pound
+*/
+const units = [
+    {
+        name: "Length (Meter/Feet)",
+        factor: 3.281
+    },
+    {
+        name: "Volume (Liters/Gallons)        factor: 3.281
+    },0.264
+]
+    {
+        name: "Mass (Kilograms/Pounds)",
+        factor: 2.204
+    }
